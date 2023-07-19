9ec9dbb5a4af2c710835af5808dc00e186219dcf
Wafaahcm <wafaahachem1431@gmail.com>
2023-07-19 19:18:24 +0100

    Added click event handling to Slider component and adjusted styles

---
+++ src/data.jsx
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://o.remove.bg/downloads/3ff558fa-9f2e-42db-a470-fed882834c5e/freestocks-_3Q3tsJ01nc-unsplash__1_-removebg-preview.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://o.remove.bg/downloads/099d9157-1878-4795-be1a-01a2126beda4/khaled-ghareeb-QN507MdnxRQ-unsplash__1_-removebg-preview.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://o.remove.bg/downloads/0139d51b-5f6e-46f4-89bf-c4322151bba7/eli-defaria-oV4PktGcXCs-unsplash-removebg-preview.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
