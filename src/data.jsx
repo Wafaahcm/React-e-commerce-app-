b66e56722840eb88db1636a29290673aa65240e7
Wafaahcm <wafaahachem1431@gmail.com>
2023-07-22 20:22:58 +0100

    Added click event handling to Slider component and adjusted styles

--- src/data.jsx
+++ src/data.jsx
@@ -1,23 +1,93 @@
+
+import image1 from './Images/image1.png';
+import image2 from './Images/image2.png';
+import image3 from './Images/image3.png';
+
+
+
+
+
+
+
+
+
+
+
+
+
 export const sliderItems = [
     {
       id: 1,
-      img: "https://o.remove.bg/downloads/3ff558fa-9f2e-42db-a470-fed882834c5e/freestocks-_3Q3tsJ01nc-unsplash__1_-removebg-preview.png",
+      img: image1,
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "f5fafd",
     },
     {
       id: 2,
-      img: "https://o.remove.bg/downloads/099d9157-1878-4795-be1a-01a2126beda4/khaled-ghareeb-QN507MdnxRQ-unsplash__1_-removebg-preview.png",
+      img: image2,
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
     },
     {
       id: 3,
-      img: "https://o.remove.bg/downloads/0139d51b-5f6e-46f4-89bf-c4322151bba7/eli-defaria-oV4PktGcXCs-unsplash-removebg-preview.png",
+      img: image3,
       title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
     },
   ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/6598273/pexels-photo-6598273.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1612336307429-8a898d10e223?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/9168240/pexels-photo-9168240.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
